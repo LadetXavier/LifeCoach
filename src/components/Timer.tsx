a6cea05d58c35e14ad343f374e2e2dd19386162e
Xavier <xavier.ladet@yahoo.fr>
2022-07-25 18:32:20 +0200

    Worked on the layout, add pause button, started the segmentation of digit.

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -19,6 +19,29 @@ const Timer = () => {
     const startTimer = () => {        
        setCurrentTime(initialTimer);                 
        setStatus(STATUS.STARTED);           
+    }   
+
+    const pauseTimer = () => {
+
+      if(status=== STATUS.STARTED) {
+         setStatus(STATUS.STOPPED);
+      }
+      else {
+        setStatus(STATUS.STARTED);
+      }
+       
+    }
+
+    const changeFocus = (e) => {
+     e.target.nextSibling.focus();
+    }
+
+    const changeFocusSeparation = (e) => {
+      e.target.nextSibling.nextSibling.focus();
+    }
+
+    const selectText = (e) => {
+      e.target.select();
     }
 
     const updateTimer = () => {
@@ -35,10 +58,20 @@ const Timer = () => {
 
   return (
     
-    <div>
+    <div className="timer">
         <h2>Timer</h2>
         <input type="text" placeholder="time" ref={TimerSettup} onChange={changeTimer}></input>
-        <input type="button" value="Start" onClick={startTimer} />
+        <div className="timer-container">
+          <input type="text" placeholder="0" maxLength="1" className="timer-input" onChange={changeFocus} onClick={selectText}></input>
+          <input type="text" placeholder="0" maxLength="1" className="timer-input" onChange={changeFocusSeparation} onClick={selectText}></input>
+          <p>:</p>
+          <input type="text" placeholder="0" maxLength="1" className="timer-input" onChange={changeFocus} onClick={selectText}></input>
+          <input type="text" placeholder="0" maxLength="1" className="timer-input" onClick={selectText}></input>
+        </div>       
+        <div className="timer-button">
+          <input type="button" value="Start" onClick={startTimer} />
+          <input type="button" value={status===STATUS.STARTED ? "Pause" : "Restart"} onClick={pauseTimer} />
+        </div>
         <p>{currentTime}</p>
     </div>
   )
